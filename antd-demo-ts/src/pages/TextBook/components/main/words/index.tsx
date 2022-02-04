21bc713dd1a213b811d039b806d4ad697f124838
Viktoria <vikalex11@mail.ru>
2022-02-04 17:09:38 +0300

    feat: add component reflection after response

--- antd-demo-ts/src/pages/TextBook/components/main/words/index.tsx
+++ antd-demo-ts/src/pages/TextBook/components/main/words/index.tsx
@@ -1,23 +1,43 @@
+/* eslint-disable array-callback-return */
+import React, { useState, useEffect } from 'react';
+
 import TextBookWordList from './wordList';
 import { getWordsGroup } from '../../../../../services/APIService';
-import { addWords } from './utils';
 
+interface Word {
+  word: string;
+}
 export const getWords = async () => {
-  const response = await getWordsGroup();
-  for (const words of response) {
-    console.log(words.word);
-    // addWords(words.word);
-  }
+  const data = await getWordsGroup();
+
+  return data;
 };
 getWords();
 
 function TextBookWordsContainer() {
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    getWords().then((data) => {
+      console.log(data[1]);
+      setWords(data);
+    });
+  }, []);
+
+  console.log(words);
+
   return (
     <div className='text__book_word-container'>
       <div className='text__book_word-greed'>
         {[...Array(20)].map((_, i) => (
           <TextBookWordList key={i} />
         ))}
+        {words.map((word: Word) => {
+          {
+            console.log(word.word);
+          }
+          <b className='text__book_word_one'>{word.word}</b>;
+        })}
       </div>
       <div className='text__book_word-details'></div>
     </div>
