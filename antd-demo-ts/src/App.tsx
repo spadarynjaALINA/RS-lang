f62c435fb428a3f6af82b5dde48b36fc0a816cf7
unknown <alinagusarik@gmail.com>
2022-02-15 20:06:00 +0300

    feat: add eslint

--- antd-demo-ts/src/App.tsx
+++ antd-demo-ts/src/App.tsx
@@ -1,19 +1,12 @@
 import React, { useState } from 'react';
- import Games from './pages/games/Games';
-import AppTextBook from './pages/TextBook';
-
-import TextBookGameCards2 from './pages/TextBook/components/main/game/gameCard'
-
-import SprintGame from './pages/GameSprint';
-
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css'
+import Games from './pages/games/Games';
+import AppTextBook from './pages/TextBook';
+import SprintGame from './pages/GameSprint';
+import './App.css';
 import Header from './pages/Header/Header';
 import StartPage from './pages/Start-page/Start-page';
-
-import Footer  from './pages/Footer/Footer';
-import AudioCall from './pages/games/audiocall/StartPageAudioCall/StartPageAudioCall';
+import Footer from './pages/Footer/Footer';
 import AudioCallGame from './pages/games/audiocall/AudioCallGame/AudioCallGame';
 
 // import TextBookHeader from './pages/TextBook/components/header';
@@ -23,37 +16,33 @@ function App() {
 
   return (
     <Router>
-      <div className='App'>
-        {/*accessToken && <h1>WE ARE AUTH!!!!!!!!!</h1>*/}
-
+      <div className="App">
+        {/* accessToken && <h1>WE ARE AUTH!!!!!!!!!</h1> */}
         <Header onLogin={setAccessToken} accessToken={accessToken} />
-        <main className='App-main'>
+        <main className="App-main">
           <Switch>
-            <Route path='/Учебник'>
+            <Route path="/Учебник">
               <TextBook accessToken={accessToken} />
             </Route>
-            <Route path='/Мини-игры/Аудиовызов'>
-              <AudioCallGame></AudioCallGame>
+            <Route path="/Мини-игры/Аудиовызов">
+              <AudioCallGame />
             </Route>
-             <Route path='/Мини-игры/Спринт'>
-              <SprintGame></SprintGame>
-              </Route>
-            <Route path='/Мини-игры'>
-              <Games></Games>
-              </Route>
-              
-            <Route path='/Статистика'>
-              <Statistic />
+            <Route path="/Мини-игры/Спринт">
+              <SprintGame />
             </Route>
-            <Route path='/'>
-              <StartPage />
-             
+            <Route path="/Мини-игры">
+              <Games />
             </Route>
 
+            <Route path="/Статистика">
+              <Statistic />
+            </Route>
+            <Route path="/">
+              <StartPage />
+            </Route>
           </Switch>
-          
         </main>
-        <Footer></Footer>
+        <Footer />
       </div>
     </Router>
   );
@@ -61,15 +50,13 @@ function App() {
 
 function TextBook(props: any) {
   return (
-    <div className='TextBookWrap'>
-      <AppTextBook accessToken={props.accessToken}></AppTextBook>
+    <div className="TextBookWrap">
+      <AppTextBook accessToken={props.accessToken} />
     </div>
   );
 }
 
-
-
 function Statistic() {
-  return <div className='StatisticWrap'> тут будет статистика</div>;
+  return <div className="StatisticWrap"> тут будет статистика</div>;
 }
 export default App;
