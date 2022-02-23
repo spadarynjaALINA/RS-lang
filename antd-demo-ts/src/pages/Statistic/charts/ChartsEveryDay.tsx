0d38577a3e14ea74107e12040558c49565c282f0
unknown <alinagusarik@gmail.com>
2022-02-23 15:04:14 +0300

    feat: add charts for long statistic

--- antd-demo-ts/src/pages/Statistic/charts/ChartsEveryDay.tsx
+++ antd-demo-ts/src/pages/Statistic/charts/ChartsEveryDay.tsx
@@ -4,33 +4,41 @@ import { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 
 import { Chart } from 'react-chartjs-2';
+import { getStatistics } from 'src/services/APIService';
 ChartJS.register(...registerables);
 
 export function ChartsEveryDay() {
-  const [charData, setCharData] = useState({ labels: ['a', 'b', ' c', 'd'],
-    datasets: [
-      {
-        
-        data: [4, 5, 7, 8],
-       
-      }], 
-  });
- 
+  const [dataArr, setDataArr] = useState([] as string[]);
+  const [wordsArr, setWordsArr] = useState([] as number[]);
   useEffect(() => {
-    setCharData({
-      labels: ['a', ' b', ' c', 'd'],
-      datasets: [
-        {          
-          data: [4, 5, 7, 8],
-         
-        },
-      ],
-   
-    });
+    const fn = async () => {
+      const a = [] as string[];const b = [] as number[];
+      await getStatistics(localStorage.getItem('userId')).then(res => {
+        res.reverse().forEach((item:any )=> {
+          a.push(item.date);
+          b.push(item.newWords);
+          setDataArr(a);
+          setWordsArr(b);         
+        });      
+      });     
+    };
+    fn();
   }, []);
+ 
+ 
   return (
     <div className='alltime-words'>
-      <Bar data={charData} />
+      <Bar data={{
+        labels:  dataArr,
+        datasets: [
+          {     
+            label:'Новых слов в день',
+            data: wordsArr,
+         
+          },
+        ],
+   
+      }} />
     </div>
   );
 }
